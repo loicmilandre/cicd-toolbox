440fce811fb1d3d5b0ddff5dfcb11875ddf1e63a
loicmilandre <40030535+loicmilandre@users.noreply.github.com>
2020-05-15 12:22:44 +0200

    Update openstack-client.sh

--- openstack-client.sh
+++ openstack-client.sh
@@ -2,16 +2,43 @@
 
 set -e
 
-if [[ $(docker ps) ]]; then
-        echo "Checking of Docker service : OK"
-else
-        echo "Checking of Docker service : ERROR because Docker service is unavailable !" && exit 1
-fi
+function system_check {
+        if [[ $(docker ps) ]]; then
+                echo "Checking of Docker service : OK"
+        else
+                echo "Checking of Docker service : ERROR because Docker service is unavailable !" && exit 1
+        fi
 
-if [[ -f "clouds.yaml" ]]; then
-        echo "Checking of clouds.yaml : OK"
-else
-        echo "Checking of clouds.yaml : ERROR because missing file in $PWD directory !" && exit 1
-fi
+        if [[ -f "clouds.yaml" ]]; then
+                echo "Checking of clouds.yaml : OK"
+        else
+                echo "Checking of clouds.yaml : ERROR because missing file in $PWD directory !" && exit 1      
+        fi
+}
 
-docker run -it --rm -v $PWD:/etc/openstack -e "OS_CLOUD=openstack" --name openstack-client loicmilandre/openstack-client:latest
+function run_client {
+        OSCLOUD=$1
+        docker run -it --rm -v $PWD:/etc/openstack -e "OS_CLOUD=$OSCLOUD" --name openstack-client loicmilandre/openstack-client:latest
+}
+
+function usage {
+	echo "Usage: ./openstack-client.sh"
+	echo "-h : display help info"
+	echo "-c [cloud name] : connect to the target cloud defined by cloud name within in clouds.yaml file" 
+}
+
+while true; do 
+    case "$1" in 
+        -c) if [ -z $2 ]; then echo "Missing cloud name" && exit 1; fi
+                system_check
+		run_client $2
+		break
+		;;
+	-h) usage
+    		exit 0
+       		;;
+	*)  usage
+		exit 0
+		;;
+    esac
+done
