cbdf2d595bd911cb8f73e97944132252fc5edbb2
loicmilandre <40030535+loicmilandre@users.noreply.github.com>
2020-05-28 12:56:43 +0200

    Update openstack-client.sh

--- openstack-client.sh
+++ openstack-client.sh
@@ -1,45 +1,4 @@
 #!/bin/bash
 
-set -e
+docker run -it --rm -v $PWD:/etc/openstack --name openstack-client-$USER loicmilandre/openstack-client:latest
 
-function system_check {
-        if [[ $(docker ps) ]]; then
-                echo "Checking of Docker service : OK"
-        else
-                echo "Checking of Docker service : ERROR because Docker service is unavailable !" && exit 1
-        fi
-
-        if [[ -f "clouds.yaml" ]]; then
-                echo "Checking of clouds.yaml : OK"
-        else
-                echo "Checking of clouds.yaml : ERROR because missing file in $PWD directory !" && exit 1      
-        fi
-}
-
-function run_client {
-        OSCLOUD=$1
-	CMD=$2
-        docker run -it --rm -v $PWD:/etc/openstack -e PYTHONWARNINGS="ignore:Unverified HTTPS request" -e "OS_CLOUD=$OSCLOUD" --name openstack-client-$USER loicmilandre/openstack-client:latest $CMD
-}
-
-function usage {
-	echo "Usage: ./openstack-client.sh"
-	echo "-h : display help info"
-	echo "-c [cloud name] : connect to the target cloud defined by cloud name within in clouds.yaml file" 
-}
-
-while true; do 
-    case "$1" in 
-        -c) if [ -z $2 ]; then echo "Missing cloud name" && exit 1; fi
-                system_check
-		run_client $2 $3
-		break
-		;;
-	-h) usage
-    		exit 0
-       		;;
-	*)  usage
-		exit 0
-		;;
-    esac
-done
